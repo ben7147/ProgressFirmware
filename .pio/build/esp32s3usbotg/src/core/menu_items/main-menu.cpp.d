74769b20a49029b122473fe3630e1853bba708c6
ben7147 <benbendomdom1000@gmail.com>
2025-06-05 22:21:10 +0200

    timeSTr undefined reference error

---
+++ .pio/build/esp32s3usbotg/src/core/menu_items/main-menu.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32s3usbotg/src/core/menu_items/main-menu.cpp.o: \
+ src/core/menu_items/main-menu.cpp
---
+++ src/modules/clock/clock.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <ESP32Time.h>
+
+
+ESP32Time rtc;
+extern String timeStr;
+
+void storeCurrentTime() {
+  char buf[9];
+  sprintf(buf, "%02d:%02d:%02d", rtc.getHour(), rtc.getMinute(), rtc.getSecond());
+  timeStr = String(buf);
+
+}
