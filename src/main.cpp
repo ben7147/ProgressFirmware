1c08229f20df6642796ed4abd0c02aaca4a6b1a3
ben7147 <benbendomdom1000@gmail.com>
2025-06-07 15:00:49 +0200

    removed unnecessary things

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,11 @@
 #include "modules/clock/clock.h"
 #include "core/display.h"
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET    -1  // Reset pin not used
 
 String currentTime;
 void setup() {
     Serial.begin(115200);
-    rtc.setTime(0, 0, 0, 1, 1, 2025);
+    // rtc.setTime(0, 0, 0, 1, 1, 2025);
     initDisplay();
     getTime();
     // displaySmallLeftTopStr("30.1 C");
